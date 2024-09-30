c967a74dbe85ac7c56ff8a49374209bcbe255aa9
suibiweng <suibi.weng@gmail.com>
2024-09-30 10:15:26 -0600

    Fire Scene Start

---
+++ Assets/Scripts/FireRoominteractive/FiresceneManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RealityEditor;
+using OpenCover.Framework.Model;
+using Unity.VisualScripting;
+
+
+
+
+public class FireSpot{
+    public Vector3 position;
+
+}
+public class FiresceneManager : MonoBehaviour
+{
+    public int CurrentStage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public List<FireSpot> FiresceneStageList;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void StageFinish(){
+
+
+
+
+
+    }
+
+
+}
