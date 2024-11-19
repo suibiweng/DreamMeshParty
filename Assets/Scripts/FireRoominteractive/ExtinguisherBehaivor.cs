435c514b2d89fdf198bf238a3938ff87a4f1de2a
horseisfish <l2248_2105@yahoo.com.tw>
2024-11-19 06:59:06 -0700

    Add extscript

---
+++ Assets/Scripts/FireRoominteractive/ExtinguisherBehaivor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtinguisherBehaivor : MonoBehaviour
+{
+    public GameObject CO2;
+    public GameObject DryPowder;
+    public GameObject Foam;
+    public GameObject Water;
+    public GameObject Wetchemical;
+    
+    enum extinguisherType
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+
+    void Update()
+    {
+        
+    }
+}
